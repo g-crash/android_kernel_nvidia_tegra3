12e1af7588ca70a0b460e282b12cf40201ae4b51
Svyatoslav Ryhel <clamor95@gmail.com>
2017-12-29 10:35:37 +0200

    asus-t30: use uarta debug port
    Change-Id: Ic5e93cfad483a9e96a80ed92672e7eae7ee8a9da

--- arch/arm/mach-tegra/board-asus-t30.c
+++ arch/arm/mach-tegra/board-asus-t30.c
@@ -333,7 +333,7 @@ static void __init uart_debug_init(void)
 {
 	int debug_port_id;
 
-	debug_port_id = uart_console_debug_init(3);
+	debug_port_id = uart_console_debug_init(0);
 	if (debug_port_id < 0)
 		return;
 	
